40764adad8085d4b573a238eb7fa4416593e60af
jose-donato <zmcdonato@gmail.com>
2022-11-10 17:49:46 +0000

    feat: initial files

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+	darkMode: ['class', '[data-theme="dark"]'],
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				"grey-50": "#f6f6f6ff",
+				"grey-100": "#eaeaeaff",
+				"grey-200": "#dcdcdcff",
+				"grey-300": "#c8c8c8ff",
+				"grey-400": "#a2a2a2ff",
+				"grey-500": "#808080ff",
+				"grey-600": "#5a5a5aff",
+				"grey-700": "#474747ff",
+				"grey-800": "#2a2a2aff",
+				"grey-850": "#131313ff",
+				"grey-900": "#070707ff",
+			}
+		},
+	},
+	plugins: [],
+};
