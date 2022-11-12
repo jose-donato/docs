21bf16f12309268c155acfa2ec58039ace03b669
andrewkenreich <andrew.kenreich@gmail.com>
2022-11-12 08:23:53 -0500

    installer page - terminal

--- src/pages/whyopenbb.tsx
+++ src/pages/whyopenbb.tsx
@@ -35,6 +35,26 @@ export default function OpenBB(): JSX.Element {
             We believe that everyone should be able to have the same tooling to do investment research. By leveraging free API tiers, we can allow users to have access to a vast range of information for free. On the other hand, we want users that are willing to subscribe to premium API keys from certain data providers to be able to take advantage of that on OpenBB Terminal - this is where we see a monetization opportunity, a revenue share with data providers for connecting our users to their resources.
             </p>
           </Link>
+          <Link to="/docs/z-guides-markdown" className="!no-underline header_docs_sdk relative w-full h-full max-w-full p-8 rounded overflow-hidden flex flex-col items-center justify-center border border-grey-400 bg-grey-900">
+            <h3 className='uppercase text-white tracking-widest font-bold text-2xl mb-2'>Importing and Exporting data</h3>
+            <p className='text-center text-sm text-grey-300'>
+            The terminal allows for users to import their own proprietary datasets to use on our econometrics menu. In addition, users are allowed to export any type of data to any type of format whether that is raw data in Excel or an image in PNG. This is also ideal for finance content creation.
+            </p>
+          </Link>
+        </div>
+        <div className='flex w-full container items-center justify-center gap-6 px-6 sm:mx-0 mt-8 md:!mt-14 lg:!mt-15 md:mb-0 flex-col lg:!flex-row z-10 lg:!translate-y-0'>
+          <Link to="/docs/z-guides-markdown" className="!no-underline header_docs_sdk relative w-full h-full max-w-full p-8 rounded overflow-hidden flex flex-col items-center justify-center border border-grey-400 bg-grey-900">
+            <h3 className='uppercase text-white tracking-widest font-bold text-2xl mb-2'>OpenBB SDK and Customizable notebook reports</h3>
+            <p className='text-center text-sm text-grey-300'>
+            Use our OpenBB Terminal environment to access raw data through a Jupyter Notebook and play with it accordingly. Or just create customizable notebook reports for your colleagues and friends doing research on a particular asset or a macroeconomic event impact on said asset. The possibilities are endless.
+            </p>
+          </Link>
+          <Link to="/docs/z-guides-markdown" className="!no-underline header_docs_sdk relative w-full h-full max-w-full p-8 rounded overflow-hidden flex flex-col items-center justify-center border border-grey-400 bg-grey-900">
+            <h3 className='uppercase text-white tracking-widest font-bold text-2xl mb-2'>Advanced user and routines</h3>
+            <p className='text-center text-sm text-grey-300'>
+            Navigate through 750+ terminal features using fast shortcuts and leverage auto-complete functionality. Jump from looking into your portfolio to comparing the financials of those companies in a few seconds. Leverage our routines implementation to run analysis while you drink your coffee or tea.
+            </p>
+          </Link>
         </div>
       </main>
   );
