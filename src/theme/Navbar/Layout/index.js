99943f0a5bf21f8c86a5a10e21913fc44b10c22d
jose-donato <zmcdonato@gmail.com>
2022-11-15 17:27:58 +0000

    fix: sidebar and toc

--- src/theme/Navbar/Layout/index.js
+++ src/theme/Navbar/Layout/index.js
@@ -43,11 +43,11 @@ export default function NavbarLayout({ children }) {
     <nav
       ref={navbarRef}
       className={clsx(
-        "border-b border-grey-600",
+        "border-b border-grey-600 lg:px-12",
         {
           header_docs_terminal: pathname.startsWith("/docs/terminal"),
           header_docs_sdk: pathname.startsWith("/docs/sdk"),
-          header_docs: pathname === "/docs/"
+          header_docs: pathname === "/docs/",
         },
         "navbar",
         "navbar--fixed-top",
