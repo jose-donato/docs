841795036091de3e0527113bf3af0b526813c4ee
jose-donato <zmcdonato@gmail.com>
2022-11-21 18:59:39 +0000

    fix: analytics and toc removed on sdk reference

--- src/theme/TOC/index.js
+++ src/theme/TOC/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import clsx from "clsx";
 import TOCItems from "@theme/TOCItems";
 import styles from "./styles.module.css";
+import { useLocation } from "@docusaurus/router";
 // Using a custom className
 // This prevents TOCInline/TOCCollapsible getting highlighted by mistake
 const LINK_CLASS_NAME = "table-of-contents__link toc-highlight";
 const LINK_ACTIVE_CLASS_NAME = "table-of-contents__link--active";
 export default function TOC({ className, ...props }) {
+  const { pathname } = useLocation()
+  if (pathname.startsWith("/sdk/reference/")) return null
   return (
     <div
       className={clsx(
